4d6ef938c81e133f0a6e6345db9030ca88e2f231
thevenetandy <thevenetandy@yahoo.fr>
2015-07-23 10:25:42 +0200

    OksApi

--- OksApi/src/fr/thevenetandy/oks/api/OksApi.java
+++ OksApi/src/fr/thevenetandy/oks/api/OksApi.java
@@ -2,11 +2,14 @@ package fr.thevenetandy.oks.api;
 
 import org.bukkit.craftbukkit.libs.jline.internal.Log;
 import org.bukkit.plugin.java.JavaPlugin;
+import fr.thevenetandy.oks.api.CommandsManager;
 
-public class OksApi extends JavaPlugin {
+public class OksApi extends JavaPlugin{
 
 	public void onEnable() {
 		Log.info("Enabling OksApi");
+
+		CommandsManager.registerNewCommand("help");
 		
 		Log.info("OksApi is enabled");
 	}
